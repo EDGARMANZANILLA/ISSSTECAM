2999673410c45b0e175c29ac875a8b6e44ad6243
Ing. Manzanilla <al045356@uacam.mx>
2020-06-15 11:01:41 -0500

    merge de Reduccion 10 porciento

---
+++ ISSSTECAM.Presupuesto.Web/Controllers/TransaccionesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ISSSTECAM.Presupuesto.Web.Controllers
+{
+    public class TransaccionesController : Controller
+    {
+        //
+        // GET: /Transaccion/
+
+        public ActionResult Index()
+        {
+
+
+            return View();
+
+        }
+
+        public JsonResult DevolverClaves()
+        {
+
+              List<string> claves = new List<string>();
+            var clavesDB = Negocios.ClavesPresupuestalesNegocios.ObtenerClavesActivas();
+         return Json(dato, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+
+
+
+
+
+    }
+}

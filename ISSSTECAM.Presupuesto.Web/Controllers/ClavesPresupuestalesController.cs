62e7d3afee25de45379345673bc8b2cd603022eb
KevinAbdValleL <14470459@itcampeche.edu.mx>
2020-06-15 10:40:00 -0500

    Validación de valores no nulos en Reducción y comentarios en Vista Transacción y ClavesPresupuestalesController.

--- ISSSTECAM.Presupuesto.Web/Controllers/ClavesPresupuestalesController.cs
+++ ISSSTECAM.Presupuesto.Web/Controllers/ClavesPresupuestalesController.cs
@@ -10,7 +10,7 @@ using System.Web.Mvc;
 namespace ISSSTECAM.Presupuesto.Web.Controllers
 {
     public class ClavesPresupuestalesController : Controller
-    {                
+    {
 
         public ActionResult Captura()
         {
@@ -21,5 +21,41 @@ namespace ISSSTECAM.Presupuesto.Web.Controllers
         {
             return PartialView();
         }
+        //Aquí es donde planeaba asignar la recepción de los datos de la vista de Transacción.
+
+        //[HttpPost]
+        //#region obtiene fecha y luego guarda, (modificar a un solo metodo)
+        //public JsonResult RecibirFechaParaGuardarNomina(int guardaQuincena, string guardaMes, int guardaAnio, int tipoNomina)
+        //{
+        //    string dato;
+        //    try
+        //    {
+        //        Nomina nueva = new Nomina();
+
+        //        nueva = (Nomina)Session["nueva"];
+        //        nueva.FechaAplicacion = DateTime.Now;
+
+        //        //La fecha (yyyy/mm/dd)
+        //        string fecha = guardaAnio + "/" + guardaMes + "/" + guardaQuincena;
+        //        nueva.FechaQuincena = Convert.ToDateTime(fecha);
+        //        nueva.Observacion = "-";
+        //        nueva.Activo = true;
+
+        //        //int tipoNomina es 1 o 2 que se encuentran asi en la DB
+        //        nueva.idTiposNominas = tipoNomina;
+
+        //        Negocios.NominaNegocios.GuardarNomina(nueva);
+        //        dato = "Guardado Existoso";
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        dato = "NO se pudo guardar la nomina, verifique e intente de nuevo por favor";
+        //    }
+        //    return Json(dato, JsonRequestBehavior.AllowGet);
+        //}
+        //#endregion
     }
 }
+
+
+
